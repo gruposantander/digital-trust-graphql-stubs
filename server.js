3b6a726c5f127a767559e7fe262af46b2852ff9e
Jorge Barrachina <nusquema@gmail.com>
2020-11-12 12:39:24 +0100

    Refactor: Added another sample to test. Code looks like more compact

--- server.js
+++ server.js
@@ -31,61 +31,31 @@ const ddbb = {
     bank_account_identifiers: new Proxy([],proxyHandlerObj)
 };
 
+
 function addPeople (filename) {
-  let {
-    username,
-    company_type,
-    company_start_date,
-    company_end_date,
-    company_trade_name,
-    company_registered_name,
-    company_country_incorporation,
-    id_documents,
-    addresses,
-    phone_numbers,
-    emails,
-    finances,
-    bank_accounts,
-    bank_account_identifiers,
-    ...basic_data} = require(filename);
+  let {username,...data} = require(filename);
   let user_id = ddbb.users.push({ username : username});
-  ddbb.personal_basic_details.push({
-    ...basic_data,
-    birthdate: new Date(basic_data.birthdate),
-    user_id: user_id
-  });
-  ddbb.business_basic_details.push({
-    company_type,
-    company_trade_name,
-    company_registered_name,
-    company_country_incorporation,
-    company_start_date: new Date(company_start_date),
-    company_end_date: new Date(company_start_date),
-    user_id: user_id
-  })
-  id_documents.map(doc => {
-    ddbb.id_documents.push({...doc, user_id: user_id})
-  });
-  addresses.map(doc => {
-    ddbb.addresses.push({...doc, user_id: user_id})
-  })
-  phone_numbers.map(doc => {
-    ddbb.phone_numbers.push({...doc, user_id: user_id})
-  });
-  emails.map(doc => {
-    ddbb.emails.push({...doc, user_id: user_id})
-  })
-  finances.map(doc => {
-    ddbb.finances.push({...doc, user_id: user_id})
-  })
-  bank_accounts.map(doc => {
-    ddbb.bank_accounts.push({...doc, user_id: user_id})
-  })
-  bank_account_identifiers.map(doc => {
-    ddbb.bank_account_identifiers.push({...doc, user_id: user_id})
+  Object.keys(ddbb).map(k => {
+    if(k !== "users") {
+      if (k === "personal_basic_details") {
+        ddbb.personal_basic_details.push({
+          ...data[k],
+          birthdate : new Date(data[k].birthdate),
+          user_id: user_id});
+      } else {
+        data[k].map(doc => {
+          // Fix date fields if any
+          Object.keys(doc).filter(field => /date/i.test(field)).map(f => {
+            doc[f] = new Date(doc[f]);
+          })
+          ddbb[k].push({
+            ...doc,
+            user_id: user_id});
+        })
+      }
+    }
   })
 
-  //console.log(`[${user_id}] added`);
 }
 
 
@@ -97,18 +67,18 @@ fs.readdir(testFolder, (err, files) => {
   const PORT = 4000;
   const app = express();
 
-  app.use('/graphql', jsonGraphqlExpress.default({
-    users: [...ddbb.users.values()],
-    personal_basic_details: [...ddbb.personal_basic_details.values()],
-    business_basic_details: [...ddbb.business_basic_details.values()],
-    id_documents: [...ddbb.id_documents.values()],
-    addresses: [...ddbb.addresses.values()],
-    phone_numbers: [...ddbb.phone_numbers.values()],
-    emails: [...ddbb.emails.values()],
-    finances: [...ddbb.finances.values()],
-    bank_accounts: [...ddbb.bank_accounts.values()],
-    bank_account_identifiers: [...ddbb.bank_account_identifiers.values()]
-  }));
-  app.listen(PORT);
-  //console.log(ddbb);
+  // app.use('/graphql', jsonGraphqlExpress.default({
+  //   users: [...ddbb.users.values()],
+  //   personal_basic_details: [...ddbb.personal_basic_details.values()],
+  //   business_basic_details: [...ddbb.business_basic_details.values()],
+  //   id_documents: [...ddbb.id_documents.values()],
+  //   addresses: [...ddbb.addresses.values()],
+  //   phone_numbers: [...ddbb.phone_numbers.values()],
+  //   emails: [...ddbb.emails.values()],
+  //   finances: [...ddbb.finances.values()],
+  //   bank_accounts: [...ddbb.bank_accounts.values()],
+  //   bank_account_identifiers: [...ddbb.bank_account_identifiers.values()]
+  // }));
+  // app.listen(PORT);
+  console.log(ddbb);
 });
